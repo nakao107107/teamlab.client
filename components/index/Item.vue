01453b3355f4d3f8e5bdf35cd9bd21950d795459
nakao107107 <imakeanewstory@gmail.com>
2019-07-21 16:54:42 +0900

    商品一覧の各商品から商品編集画面へのリンク作成

--- components/index/Item.vue
+++ components/index/Item.vue
@@ -7,6 +7,9 @@
           <img :src="item.images[0].url"
                alt="product image"
                class="img-fluid">
+          <div class="product-hover-overlay">
+            <a :href="`/${item.id}/edit`" class="product-hover-overlay-link"></a>
+          </div>
         </div>
         <div class="py-2">
           <h3 class="h6 text-uppercase mb-1">{{item.name}}</h3>
